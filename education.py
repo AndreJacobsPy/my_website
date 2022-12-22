eaba92b21b8c432c2fa145f072319f77d1e97a1f
Andre Jacobs <andrejacobs@Andres-MBP.home>
2022-12-22 13:51:27 -0600

    added college experience

--- education.py
+++ education.py
@@ -5,6 +5,13 @@ def college() -> None:
         '''
         ### College
 
-        Arkansas Tech University | BBA in Business Data Analytics
+        Arkansas Tech University | BBA in Business Data Analytics | 2018 - 2022
+        - College golfer -> 4 Years
+        - Classwork using Python, R, SAS, SQL, Excel, UNIX, C++
+        - Coursework in Programming, Statistics, Data Analysis, Database Development, Big Data
+
+        University of North Alabama | BBA in Data Analytics
+        - College golfer -> 1 Year (Transfered for final year of eligibility)
+        - Coursework in Time Series Analysis, Machine Learning, Object Oriented Programming
         '''
     )
